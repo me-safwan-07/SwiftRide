6b525d83e266b0311d4ea7805830c3a64b1c402f
me-safwan-07 <mesafwan07@gmail.com>
2025-01-15 18:29:30 +0530

    add: create user model with authentication methods and validation

---
+++ server/models/user.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        firsname: {
+            type: String,
+            required: true,
+            minlength: [3, 'First name must be at least 3 characters long'],
+        },
+        lastname: {
+            type: String,
+            required: true,
+            minlength: [3, 'Last name must be at least 3 characters long'],
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/\S+@\S+\.\S+/, 'Invalid email format'],
+        minlength: [5, 'Email must be at least 5 characters long']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [8, 'Password must be at least 8 characters long'],
+        match: [/[a-z]/, 'Password must contain at least one lowercase letter'],
+        match: [/[A-Z]/, 'Password must contain at least one uppercase letter'],
+        match: [/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character'],
+        select: false,
+    },
+    sockedId: {
+        type: String,
+    }
+});
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id}, process.env.JWt_SECRET, { expiresIn: '24h' });
+    return token;
+}
+
+userSchema.methods.comparePassword = async function () {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10)
+}
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
