da9b823cb9225e1f79797cc243448f89e3c2e7e2
me-safwan-07 <mesafwan07@gmail.com>
2025-01-15 15:14:31 +0530

    add CORS middleware and connect to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const express = require('express');
-const db = require('./db/db');
+const cors = require('cors');
+const connectDB = require('./db/db');
+
+
 const app = express();
 
-db();
+// Connect to MongoDB
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
