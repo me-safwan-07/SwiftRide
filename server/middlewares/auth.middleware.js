5e715670faa6ca6c096bf4719e840a486ab849c6
me-safwan-07 <mesafwan07@gmail.com>
2025-01-15 15:23:29 +0530

    add authentication middleware to verify JWT tokens

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.authUser = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
+            req.user = decoded.user;
+
+            return next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+};
