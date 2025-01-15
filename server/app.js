edfcb757c2ff6a616786c23a98337942b3a972b4
me-safwan-07 <mesafwan07@gmail.com>
2025-01-15 14:40:49 +0530

    add mongoose connection and update dependencies

--- server/app.js
+++ server/app.js
@@ -3,9 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
-
+const db = require('./db/db');
 const app = express();
 
+db();
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
