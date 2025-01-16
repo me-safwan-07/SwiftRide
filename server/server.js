531ab20d3a4fa26eb88a3549827b6c4d1181f449
me-safwan-07 <mesafwan07@gmail.com>
2025-01-16 22:50:21 +0530

    fix: remove redundant URL scheme from server startup log

--- server/server.js
+++ server/server.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}/`)
+    console.log(`Server is running on port http://localhost:${PORT}`)
 })
