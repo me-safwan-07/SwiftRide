6d59867d5a405c18d86b4b4c9cf380707956097d
me-safwan-07 <mesafwan07@gmail.com>
2025-01-16 22:50:09 +0530

    fix: simplify MongoDB connection logic in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try {
-        const connect = await mongoose.connect('mongodb://localhost:27017/SwiftRide')
+        await mongoose.connect('mongodb://localhost:27017/SwiftRide')
         console.log('MongoDB Connected...');
     } catch (err) {
         console.error(err.message);
