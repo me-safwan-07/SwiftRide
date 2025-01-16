5a3cc15c4b262330494587752d9845509efbbf22
me-safwan-07 <mesafwan07@gmail.com>
2025-01-16 22:50:15 +0530

    add: integrate user routes into the main application

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./db/db');
 
+const userRoutes = require('./routes/user.routes')
 
 const app = express();
 
@@ -17,5 +18,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
+
+app.use('/api/user', userRoutes)
 
 module.exports = app;
